ea2de054471057b352cb611d118979e710e8ad35
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-07 08:14:09 -0300

    Aula 02

---
+++ aula-02.js
@@ -0,0 +1,10 @@
+// const é uma constante, nada mudará essa váriavel (numero1 com o valor de 1)
+const numero1 = 1;
+// let é modificável, pode ser alterado o valor
+let numero2 = 2;
+
+if(numero1 === 1){
+    numero2 = 6
+}
+
+console.log('Resultado: ', numero2);
