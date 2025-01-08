beab6e08c2c302a97c2aa5bb08bb44721d131fc4
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-08 08:22:18 -0300

    Aula 08

---
+++ aula-08.js
@@ -0,0 +1,11 @@
+const salario = '103';
+const salarioNumero = Number(salario);
+
+// 103 como string
+console.log('Salario: ', salario);
+
+//103 como numero
+console.log('Salario numero: ', salarioNumero);
+
+//103 com 2 numeros decimais
+console.log('Salario numero: ', salarioNumero.toFixed(2));
