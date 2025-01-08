7e0a25fe98bf581f84ce796352d8b60cd1ee1389
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-08 08:00:02 -0300

    Aula 06

---
+++ aula-06.js
@@ -0,0 +1,8 @@
+let idade = 1;
+
+// do while = ele passa primeiro a informação mesmo sendo falsa e depois ele verifica se deve passar de novo
+
+do{
+    console.log('Passou!', idade);
+    idade = idade + 1;
+}while (idade <= 18);
