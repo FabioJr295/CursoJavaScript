c0741fc62a3a11d3d4bd4ce55b9be23c43d9143c
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-08 07:55:32 -0300

    Aula 05

---
+++ aula-05.js
@@ -0,0 +1,10 @@
+let idade = 1;
+
+// while = enquanto || !Cuidado pra não entrar em um loop eterno e travar o pc!
+
+while(idade <= 18){
+    console.log('Passou!', idade)
+    idade = idade + 1;
+}
+
+// Enquanto a idade for menor ou igual à 18, o looping irá continuar, assim que chgar aos 18 anos, o loop é finalizado!
