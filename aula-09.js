999f40da4974492fd1df237463e7cef62c20125f
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-08 08:27:42 -0300

    Aula 09

---
+++ aula-09.js
@@ -0,0 +1,14 @@
+// funciton = só executa as variaveis quando elas são chamadas
+// return = ele retorna as variaveis que eu escolher
+
+function soma(item1, item2){
+    return item1 + item2;
+}
+
+function diminuir(item1, item2){
+    return item1 - item2;
+}
+
+console.log('Soma 1: ', soma(5, 6));
+console.log('Soma 2: ', soma(700, 100));
+console.log('Soma 3: ', diminuir(900, 300));
