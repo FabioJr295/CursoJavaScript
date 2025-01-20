7441c2df1767bef20a40ab7fbfeb0264aceec2ad
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-20 08:05:55 -0300

    Aula 14

---
+++ aula-14.js
@@ -0,0 +1,16 @@
+const mensagem = 'Olá Mundo!'
+const mensagem2 = 'Mundo Olá!'
+
+// lenght = ele conta os carácteres da frase
+//console.log(mensagem.length);
+
+// concat = ele junta as duas mensagens, existe outra forma de concatenar que é utilizando o + em vez de usar o próprio "concat"
+//console.log(mensagem.concat(mensagem2));
+
+// substring (numero desejado) = ele vai executar a frase a partir da letra em que eu escolhi começar pelo número
+// se caso existir uma virgula (3, 2), ele pegará a partir da terceira letra e contar mais 2 casas e formará 3 letras da palavra
+//console.log(mensagem.substring(3));
+
+// ele quebra a mensagem com os espaços, ou co alguma letra que podemos colocar (lembrando que ele apagará a letra), quebra em array
+//console.log(mensagem.split(''));
+
