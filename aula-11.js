71ad1239c10cee56f9deb740415e80ef5a4c89fb
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-20 07:24:50 -0300

    Aula 11

---
+++ aula-11.js
@@ -0,0 +1,12 @@
+// + Soma
+// - Subtração
+// / Divisão
+// * Multiplicação
+
+// % Resto
+console.log(11 % 2);
+// O resto é 1, pois é o que sobra
+
+// ** elevado
+console.log(11 ** 2);
+// Seria 11 elevado a 2, ou seja 11x11
