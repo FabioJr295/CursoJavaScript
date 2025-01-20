f0027ffa80eccb6f5c45d99d443d8142316b6a37
Fabio_Seripieri <nenijr31@gmail.com>
2025-01-20 07:19:54 -0300

    Aula 10

---
+++ aula-10.js
@@ -0,0 +1,12 @@
+ const consumoAutomovel = 12;
+ 
+ function gastoCombustivel(tempoGasto, velocidadeMedia){
+    const distancia = velocidadeMedia * tempoGasto;
+    const consumoFinal = distancia / consumoAutomovel;
+
+    return consumoFinal.toFixed(3);
+ } 
+
+console.log(gastoCombustivel(10, 85));
+console.log(gastoCombustivel(2, 92));
+console.log(gastoCombustivel(22, 67));
